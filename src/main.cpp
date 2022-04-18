2e445997eb39db38b2e2757211ec734b112d1df9
Konrad Szychowiak <konrad.szychowiak@gmail.com>
2022-04-18 20:44:22 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// todo: sending mutex
+
+// todo: status
+
+/**
+ * todo: should start both threads
+ * @return
+ */
+int main()
+{
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
